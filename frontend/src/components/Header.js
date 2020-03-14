54ab6b1f2147bdc0bf1836c4872b5dfc9f942728
Felipe <fruiz.rob@gmail.com>
2020-03-14 15:54:00 -0300

    feat: now waiting patients show like a notification

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,9 +11,17 @@ export default function Header(props) {
             <div className="container">
                 <Link className="title" to="/">Unidad de Emergencia</Link>
                 <div className="menu">
-                    <p>Iniciar jornada</p>
-                    <p>Generar reporte</p>
-                    <p onClick={categorizationStore.toggleCategorizationSection}>Categorización</p>
+                    <div className="menu-option">
+                        <p>Enfermer@, Moises Moraga</p>
+                    </div>
+                    <div className="menu-option">
+                        <p>Generar reporte</p>
+                    </div>
+                    <div onClick={categorizationStore.toggleCategorizationSection} className="menu-option">
+                        <p>Categorización</p>
+                        {/* this will be the number of waiting patients */}
+                        <p className="categorization-notification">4</p> 
+                    </div>
                 </div>
             </div>
         </header>
